ae8f86a42be82e63d0442c16c678d4186f57b212
DheerajMantri <108468370+DheerajMantri@users.noreply.github.com>
2025-03-21 18:05:02 +0000

    St

--- 01_basics/string.js
+++ 01_basics/string.js
@@ -1,9 +1,17 @@
-const name="Dheeraj"
-const repoCount=50
+//const name="Dheeraj"
+//const repoCount=50
 
 // Add a String
-console.log(name +repoCount);// this is old Process
+//console.log(name +repoCount);// this is old Process
 
 // Add Sring with new Process
-console.log(`Hello my name is ${name} and My Repo Count is ${repoCount}`)
+//console.log(`Hello my name is ${name} and My Repo Count is ${repoCount}`)
 
+const word=new String("Dheeraj Mantri")
+//console.log(word.anchor())
+//console.log(word.at(3))
+//console.log(word.big())
+//console.log(word.blink())
+//console.log(word.bold())
+//console.log(word.isWellFormed())
+console.log(word.strike())
