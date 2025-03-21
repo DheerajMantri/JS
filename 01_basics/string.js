21a025f9e5c59afafb705228c085587ee6733baf
DheerajMantri <108468370+DheerajMantri@users.noreply.github.com>
2025-03-21 15:50:58 +0000

    String

---
+++ 01_basics/string.js
@@ -0,0 +1,9 @@
+const name="Dheeraj"
+const repoCount=50
+
+// Add a String
+console.log(name +repoCount);// this is old Process
+
+// Add Sring with new Process
+console.log(`Hello my name is ${name} and My Repo Count is ${repoCount}`)
+
