c7de69e8a63558dcf1155a24fc6293aafe3ff447
DheerajMantri <108468370+DheerajMantri@users.noreply.github.com>
2025-03-22 11:27:15 +0000

    Arrya

---
+++ 02_basics/Array.js
@@ -0,0 +1,25 @@
+const Arry=[2,85,9,4,6]
+//console.log(Arry)
+//console.log(Arry[2])
+
+//Arry.push(220) // Push is use to add element in Arrya list
+//console.log(Arry)
+//Arry.pop() // POP is use to remove last element from Array List
+//console.log(Arry)
+//Arry.unshift(524)  // unshift is use to add elemant in front in array list
+//console.log(Arry)
+//Arry.shift()  // shift is use to remove first element from the arry list
+//console.log(Arry)
+//console.log(Arry.includes(5))
+//console.log(Arry.includes(9))
+//console.log(Arry.indexOf(55))
+//console.log(Arry.indexOf(85))
+
+//const newArr=Arry.join()
+//console.log(Arry)
+//console.log(typeof newArr)
+
+
+// Slice, Splice
+
+console.log("A",Arry)
