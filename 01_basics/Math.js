ad144976ae22896d9dda28be2953191226da29a0
DheerajMantri <108468370+DheerajMantri@users.noreply.github.com>
2025-03-22 02:54:55 +0000

    Number

---
+++ 01_basics/Math.js
@@ -0,0 +1,16 @@
+//console.log(Math.abs(-4))   //abs() use for change the negative to positive value
+//console.log(Math.round(45.8))  // RoundOFF
+//console.log(Math.ceil(45.1))  
+//console.log(Math.floor(45.9))  
+//console.log(Math.min(54,89,45,1))
+//console.log(Math.max(54,89,45,1))
+
+//console.log(Math.random())
+// Random is give he value between 0 to 1 only
+// TO avoid 0 value add 1
+//console.log((Math.random()*10)+1)
+
+const min=10
+const max=20
+
+console.log(Math.floor(Math.random()*(max-min +1))+min )
