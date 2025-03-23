d8519a1e85f905eac3427958afd4193184335233
DheerajMantri <108468370+DheerajMantri@users.noreply.github.com>
2025-03-23 17:35:18 +0000

    Array

--- 02_basics/Array.js
+++ 02_basics/Array.js
@@ -1,4 +1,4 @@
-const Arry=[2,85,9,4,6]
+//const Arry=[2,85,9,4,6]
 //console.log(Arry)
 //console.log(Arry[2])
 
@@ -21,5 +21,26 @@ const Arry=[2,85,9,4,6]
 
 
 // Slice, Splice
+//const Arry=[2,85,9,4,6]
+//console.log("A", Arry)   
+//const my1 = Arry.slice(1,3)
+//console.log(my1)
+//const my2 = Arry.splice(1,3)  // Splice is use remove element from orignal array
+//console.log(my2)
+//console.log()
 
-console.log("A",Arry)
+//const Family1=["Shivpradad","Shrikanwar","Dheeraj"]
+// const Family2=["Bang","Laturiya","Chandak"]
+// const Family3=["I Love My Family"]
+
+//Family1.push(Family2)
+//console.log(Family1)
+//const AllFamily=Family1.concat(Family2)  // In Concat method we can add only two Array
+//console.log(AllFamily)
+
+//const AllFamily=[...Family1,...Family2,...Family3]  // This is Spread Method means Add two or more Array
+//console.log(AllFamily)
+
+const Array=[1,2,3,[8,9,5],7,[8,7,3,7]]
+const realArray=Array.flat(Infinity)
+console.log(realArray)
