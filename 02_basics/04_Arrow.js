049d25bddca58224dec6f123d5950d835051427c
DheerajMantri <108468370+DheerajMantri@users.noreply.github.com>
2025-03-31 07:33:39 +0000

    Arrow

--- 02_basics/04_Arrow.js
+++ 02_basics/04_Arrow.js
@@ -1,13 +1,42 @@
 const user={
-    username:"Dheeraj",
-    Price:1000,
+    userrname:"Dheeraj",
+    price:1000,
 
-    WelecomeMessage:function(){
-        console.log(`${this},Welcome to the website`)
-       //console.log(this);
+    welcomeMessage: function(){
+        console.log(`${this.userrname}, Weclcome to webSite`);
+        //console.log(this)
     }
 }
 
-user.WelecomeMessage()
-user.username="Raju"
-user.WelecomeMessage()
+//console.log(this)
+//user.welcomeMessage()
+//user.userrname="Sneha"
+//user.welcomeMessage()
+
+//function chai(){
+  //  let userrname="Dheeraj"
+    //console.log (this.userrname)
+//}
+//chai()
+
+//const chai=function(){
+  //  let username="Dheeraj"
+   // console.log(this.username)
+//}
+const chai=()=>{   
+    let username="Dheeraj"
+    console.log(this)
+}
+chai()
+
+// ()=>{}  This Arrow function syntax
+
+const addTwo=(num1,num2)=>{
+    return num1+num2
+}
+console.log(addTwo(3,4))
+
+//This another method
+const addnumber=(num1,num2)=>  num1+num2  
+console.log(addnumber(3,4))
+
